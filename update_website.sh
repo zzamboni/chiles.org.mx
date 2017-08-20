2fa2732414ae80f7e6f96ae0055700133eeffbfe
Susana Soriano Ramirez <sus@cuper.local>
2017-08-20 17:38:12 +0200

    Update website

--- update_website.sh
+++ update_website.sh
@@ -3,5 +3,5 @@ OUTDIR=docs
 LOCAL_GHOST="http://localhost:2368/"
 wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST && \
 git add . && \
-git ci -m 'Update website' &&  \
+git commit -m 'Update website' &&  \
 git push
